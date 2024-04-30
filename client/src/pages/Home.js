36996b38014b2022916d06baf0b285a61881c127
tharushi <thswalpola@gmail.com>
2024-04-30 23:43:12 +0530

    changes by tharushi

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to Our Website!</h1>
+            <p>Explore our amazing collection of photos and videos.</p>
+            Add more content as needed
+        </div>
+    );
+}
+
+export default Home;
