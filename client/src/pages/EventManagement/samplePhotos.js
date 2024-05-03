038e72e0c478f9c85161de5e2604218b90fbeeb0
panchaliSam <panchalisam20030117@gmail.com>
2024-05-03 19:59:06 +0530

    remove select option

--- client/src/pages/EventManagement/samplePhotos.js
+++ client/src/pages/EventManagement/samplePhotos.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle as fasCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 const SelectPhotos = () => {
     const { eventId } = useParams();
     const [eventPhotos, setEventPhotos] = useState([]);
-    const [selectedPhotos, setSelectedPhotos] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -27,40 +24,17 @@ const SelectPhotos = () => {
         fetchEventPhotos();
     }, [eventId]);
 
-    const handleSelect = (photo) => {
-        setSelectedPhotos(prevSelectedPhotos => {
-            if (prevSelectedPhotos.includes(photo)) {
-                return prevSelectedPhotos.filter(selectedPhoto => selectedPhoto !== photo);
-            } else {
-                return [...prevSelectedPhotos, photo];
-            }
-        });
-    };
-
     return (
         <div className="home">
             <div className="photos" style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))', gap: '15px', marginLeft: '250px' }}>
                 {error && <p>Error: {error}</p>}
                 {eventPhotos.map((photo, index) => (
                     <div key={index} style={{ position: 'relative', display: 'inline-block' }}>
-                        <div style={{ position: 'relative', cursor: 'pointer' }} onClick={() => handleSelect(photo)}>
-                            <img
-                                src={photo}
-                                alt={`Event ${index + 1}`}
-                                style={{ width: '200px', height: '150px' }}
-                            />
-                            <FontAwesomeIcon
-                                icon={fasCheckCircle}
-                                size="2x"
-                                style={{
-                                    position: 'absolute',
-                                    top: '-2px',
-                                    right: '0px',
-                                    color: selectedPhotos.includes(photo) ? '#37ff00' : 'transparent',
-                                    fontWeight: 'bold',
-                                }}
-                            />
-                        </div>
+                        <img
+                            src={photo}
+                            alt={`Event ${index + 1}`}
+                            style={{ width: '200px', height: '150px' }}
+                        />
                     </div>
                 ))}
             </div>
