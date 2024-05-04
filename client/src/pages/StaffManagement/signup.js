3f92c495e603bb94c48f8330142ab83f660cbbe5
Madara <madarameegama7@gmail.com>
2024-05-04 18:49:57 +0530

    Frontend changes in Staff Management

---
+++ client/src/pages/StaffManagement/signup.js
@@ -0,0 +1,13 @@
+// Sign Up Page
+import React from 'react';
+import SignUp from '../../components/StaffManagement/signup';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
